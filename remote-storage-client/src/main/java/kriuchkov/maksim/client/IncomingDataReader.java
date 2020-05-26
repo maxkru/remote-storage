fec7c7968bff6c61a8994fea3f545882349c8279
maxkru <max_kru@hotmail.com>
2020-05-26 13:39:58 +0300

    Client: starting to fix separated command case

--- remote-storage-client/src/main/java/kriuchkov/maksim/client/IncomingDataReader.java
+++ remote-storage-client/src/main/java/kriuchkov/maksim/client/IncomingDataReader.java
@@ -12,24 +12,38 @@ import java.util.Queue;
 
 public class IncomingDataReader extends ChannelInboundHandlerAdapter {
 
-    private Queue<String> commandQueue;
-    private ByteBuf dataBuf;
+    private final Queue<String> commandQueue;
+    private final ByteBuf incompleteCommandBuf;
+    private final ByteBuf dataBuf;
+
+    private int numberOfMoreCommandBytesExpected;
 
     public IncomingDataReader() {
         commandQueue = new LinkedList<>();
+        incompleteCommandBuf = ByteBufAllocator.DEFAULT.buffer(1024 * 256);
         dataBuf = ByteBufAllocator.DEFAULT.buffer(1024 * 1024 * 8);
+        numberOfMoreCommandBytesExpected = 0;
     }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf in = (ByteBuf) msg;
         try {
             while (in.isReadable()) {
+
+                if (numberOfMoreCommandBytesExpected > 0) {
+                    incompleteCommandBuf.writeBytes(in, numberOfMoreCommandBytesExpected);
+                }
+
                 byte firstByte = in.readByte();
                 if (firstByte == Protocol.COMMAND_SIGNAL_BYTE) {
                     int commandByteLength = in.readInt();
-                    if (in.isReadable(commandByteLength)) {
+                    if (in.readableBytes() >= commandByteLength) {
                         String command = in.readCharSequence(commandByteLength, StandardCharsets.UTF_8).toString();
                         commandQueue.add(command);
+                    } else {
+                        numberOfMoreCommandBytesExpected = in.readableBytes() - commandByteLength;
+
                     }
                 } else if (firstByte == Protocol.DATA_SIGNAL_BYTE) {
                     while (in.isReadable()) {
