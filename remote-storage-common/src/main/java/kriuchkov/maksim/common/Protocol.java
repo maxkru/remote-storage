9c1861a3f822f09327069ccbeaedb60f9b2b1d95
maxkru <max_kru@hotmail.com>
2020-05-31 19:58:43 +0300

    Protocol: added list of possible commands

--- remote-storage-common/src/main/java/kriuchkov/maksim/common/Protocol.java
+++ remote-storage-common/src/main/java/kriuchkov/maksim/common/Protocol.java
@@ -3,4 +3,26 @@ package kriuchkov.maksim.common;
 public class Protocol {
     public static final byte COMMAND_SIGNAL_BYTE = 1;
     public static final byte DATA_SIGNAL_BYTE = 2;
+
+    public static final String[] POSSIBLE_CLIENT_COMMANDS = {
+            "HELLO",
+            "LIST",
+            "FETCH",
+            "STORE",
+            "DELETE",
+            "RENAME",
+            "AUTH",
+            "REGISTER-USER",
+    };
+
+    public static final String[] POSSIBLE_SERVER_RESPONSES = {
+            "HELLO-RESP",
+            "LIST-RESP",
+            "FETCH-RESP",
+            "STORE-RESP",
+            "DELETE-RESP",
+            "RENAME-RESP",
+            "AUTH-RESP",
+            "REGISTER-USER-RESP",
+    };
 }
